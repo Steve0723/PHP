3f0da9270a592e65444d052e96d996cbd0c5960d
Steve0723 <50825347+Steve0723@users.noreply.github.com>
2019-11-29 15:29:04 +0800

    Create ba06ec2124b6c0d840db95b7d4dadd31.php

---
+++ runtime/temp/ba06ec2124b6c0d840db95b7d4dadd31.php
@@ -0,0 +1,150 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:4:{s:66:"D:\wamp\gdhb.com\public/../application/admin\view\index\index.html";i:1530582314;s:51:"D:\wamp\gdhb.com\application\admin\view\layout.html";i:1530578925;s:58:"D:\wamp\gdhb.com\application\admin\view\public\header.html";i:1530709387;s:58:"D:\wamp\gdhb.com\application\admin\view\public\footer.html";i:1530578588;}*/ ?>
+<!--这是模板布局文件-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+    <title>管理后台首页</title>
+    <link rel="stylesheet" href="/statics/style/bootstrap.css"/>
+    <link rel="stylesheet" href="/statics/style/bootstrap-theme.css"/>
+    <link rel="stylesheet" href="/statics/style/index.css"/>
+    <script src="/statics/js/jquery.min.js"></script>
+    <script src="/statics/js/bootstrap.min.js"></script>
+    <script src="/statics/js/vue.js"></script>
+    <script src="/statics/js/axios.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-toggle="collapse" data-target="#topMenu">
+                <i class="icon-bar"></i>
+                <i class="icon-bar"></i>
+                <i class="icon-bar"></i>
+            </button>
+            <a href="<?php echo url('admin/index/index'); ?>" class="navbar-brand">内容管理后台</a>
+        </div>
+        <div class="navbar-collapse collapse" id="topMenu">
+            <ul class="navbar-nav nav">
+                <li class="active"><a href="#">服务器信息</a></li>
+                <li><a href="#">用户管理</a></li>
+                <li><a href="#">分类管理</a></li>
+                <li><a href="#">产品管理</a></li>
+                <li><a href="#">内容管理</a></li>
+            </ul>
+            <ul class="navbar-nav nav navbar-right">
+                <li><a href="#">欢迎<?php echo \think\Session::get('admins.username'); ?>登陆</a></li>
+                <li><a href="#" data-toggle="dropdown">操作<i class="caret"></i></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">个人信息</a></li>
+                        <li><a href="#">修改密码</a></li>
+                        <li class="divied"></li>
+                        <li><a href="<?php echo url('admin/Login/loginOut'); ?>">退出登陆</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid contentbox" style="margin-top:51px;">
+    <div class="row">
+        <div class="col-sm-2">
+            <ul class="nav navbar-inverse">
+                <li>
+                    <a href="#usermanage" data-toggle="collapse" class="collapsed">
+                        <i class="glyphicon glyphicon-user"></i>&nbsp;&nbsp;用户管理<i class="glyphicon glyphicon-chevron-up pull-right"></i>
+                    </a>
+                    <div class="list-group collapse" id="usermanage">
+                        <a href="<?php echo url('admin/User/add'); ?>" class="list-group-item"><i class="glyphicon glyphicon-plus"></i>添加用户</a>
+                        <a href="<?php echo url('admin/User/index'); ?>" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i>用户列表</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-lock"></i>用户权限</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#menumanage" data-toggle="collapse" class="collapsed">
+                        <i class="glyphicon glyphicon-menu-hamburger"></i>&nbsp;&nbsp;菜单管理<i class="glyphicon glyphicon-chevron-up pull-right"></i>
+                    </a>
+                    <div class="list-group collapse" id="menumanage">
+                        <a href="<?php echo url('admin/Menu/index'); ?>" class="list-group-item"><i class="glyphicon glyphicon-plus"></i>后台菜单</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i>用户列表</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-lock"></i>用户权限</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#cotnentmanage" data-toggle="collapse" class="collapsed">
+                        <i class="glyphicon glyphicon-book"></i>&nbsp;&nbsp;内容管理<i class="glyphicon glyphicon-chevron-up pull-right"></i>
+                    </a>
+                    <div class="list-group collapse" id="cotnentmanage">
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-plus"></i>添加用户</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i>用户列表</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-lock"></i>用户权限</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#catemanage" data-toggle="collapse" class="collapsed">
+                        <i class="glyphicon glyphicon-list-alt"></i>&nbsp;&nbsp;栏目管理<i class="glyphicon glyphicon-chevron-up pull-right"></i>
+                    </a>
+                    <div class="list-group collapse" id="catemanage">
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-plus"></i>添加用户</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i>用户列表</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-lock"></i>用户权限</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#productmanage" data-toggle="collapse" class="collapsed">
+                        <i class="glyphicon glyphicon-shopping-cart"></i>&nbsp;&nbsp;产品管理<i class="glyphicon glyphicon-chevron-up pull-right"></i>
+                    </a>
+                    <div class="list-group collapse" id="productmanage">
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-plus"></i>添加用户</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i>用户列表</a>
+                        <a href="#" class="list-group-item"><i class="glyphicon glyphicon-lock"></i>用户权限</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+
+        <div class="col-sm-10">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    服务器信息
+                </div>
+                <table class="table table-bordered table-responsive">
+                    <tbody>
+                    <tr>
+                        <td>当前域名</td>
+                        <td><?php echo \think\Request::instance()->server('server_name'); ?></td>
+                    </tr>
+                    <tr>
+                        <td>服务器IP</td>
+                        <td><?php echo \think\Request::instance()->server('server_addr'); ?></td>
+                    </tr>
+                    <tr>
+                        <td>客户端IP</td>
+                        <td><?php echo \think\Request::instance()->server('remote_addr'); ?></td>
+                    </tr>
+                    <tr>
+                        <td>服务器信息</td>
+                        <td><?php echo \think\Request::instance()->server('server_software'); ?></td>
+                    </tr>
+                    <tr>
+                        <td>客户端浏览器信息</td>
+                        <td><?php echo \think\Request::instance()->server('http_user_agent'); ?></td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+
+
+
+
+</div>
+</div>
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+    <a href="#">广州粤嵌科技有限公司版权所有&copy;</a>
+</nav>
+</body>
+</html>
